a06715b176c4d41df703457f981de2738764b2eb
shashrgowda <shashankr6@gmail.com>
2020-05-13 22:33:00 +0530

    Init commit with basic structure

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ redirect: true });
+      }, 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          Error occured. <Link to="/">Click here</Link> to redirect to the
+          homepage or wait for 5 seconds.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
